a240139b89b4ccad7060020f96e8816dc3e8503e
DeGatchi <DeGatchi@users.noreply.github.com>
2022-02-09 00:14:58 +1100

    add: UniswapV3 `noDelegateCall()`

--- src/Attack.sol
+++ src/Attack.sol
@@ -5,4 +5,10 @@ contract Attack {
     function attack(address attacking) public payable {
         selfdestruct(payable(attacking));
     }
+
+    uint256 public num = 10;
+
+    function work(uint256 _num) public {
+        num = _num;
+    }
 }
