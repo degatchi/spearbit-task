32e37cf91dd67403bee383e75f26ed47c051ace8
DeGatchi <DeGatchi@users.noreply.github.com>
2022-11-01 17:51:27 +1100

    Update Implementation.sol

--- src/Implementation.sol
+++ src/Implementation.sol
@@ -6,15 +6,15 @@ pragma solidity ^0.8.9;
 /// The implementation contract for the Proxy (see: `Proxy.sol`).
 ///
 /// Only deployed once and the implementation is reused by all proxy contracts.
-contract Implementation {
+library Implementation {
 
-    function callContract(address a, bytes calldata _calldata) payable external returns (bytes memory) {
+    function callContract(address a, bytes calldata _calldata) external returns (bytes memory) {
         (bool success , bytes memory ret) =  a.call{value: msg.value}(_calldata);
         require(success);
         return ret;
     }
 
-    function delegatecallContract(address a, bytes calldata _calldata) payable external returns (bytes memory) {
+    function delegatecallContract(address a, bytes calldata _calldata) external returns (bytes memory) {
         (bool success, bytes memory ret) =  a.delegatecall(_calldata);
         require(success);
         return ret;
